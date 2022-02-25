dc345208c90de0c82a62db762226bc30363b65a3
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-02-25 15:32:06 -0300

    Added timer util

---
+++ Assets/Utils/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Timer
+{
+    public float time { get; set; }
+
+    public bool CalcTime(float maxTime)
+    {
+        time += Time.deltaTime;
+        if (time >= maxTime)
+        {
+            time = 0;
+            return true;
+        }
+        return false;
+    }
+}
