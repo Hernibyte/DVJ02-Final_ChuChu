6e2838c4d999ab14b66c2f88be576ab0d7b55eab
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-02-25 15:33:15 -0300

    Added camera get point system

---
+++ Assets/Scripts/CameraGetWorldPoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGetWorldPoint : MonoBehaviour
+{
+    [SerializeField] LayerMask terrainMask;
+    public CameraEvent e_CameraPointClick = new CameraEvent();
+
+    Camera cam;
+
+
+    void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        Vector3 mousePos = Input.mousePosition;
+
+        Ray ray = cam.ScreenPointToRay(mousePos);
+        Debug.DrawRay(ray.origin, ray.direction * 10000, Color.yellow);
+
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 10000, terrainMask))
+            {
+                e_CameraPointClick?.Invoke(hit.point);
+            }
+        }
+    }
+}
