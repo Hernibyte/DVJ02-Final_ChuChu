7aec8efc37e51f0c48f9265daa970d0a5cd7ac38
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-02-25 15:33:26 -0300

    Added game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] CameraGetWorldPoint m_cam;
+    [SerializeField] PlayerMovement m_playerMovement;
+
+    void Start()
+    {
+        m_cam.e_CameraPointClick.AddListener(m_playerMovement.MoveTo);
+    }
+}
