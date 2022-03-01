18269c246e1ae8f5cbc23cbb0d0cebf27a27554a
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-02-28 21:03:11 -0300

    Added mymask utils class

---
+++ Assets/Utils/MyMask.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMask 
+{
+    public static bool Contains(LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+}
