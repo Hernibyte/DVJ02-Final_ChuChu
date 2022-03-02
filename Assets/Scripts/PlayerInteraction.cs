918ad63f42f96b3e4e303b602bedde2763a01a3a
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-03-01 21:03:29 -0300

    Added player world interactions

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] PlayerStats stats;
+    [SerializeField] LayerMask pointMask;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (MyMask.Contains(pointMask, col.gameObject.layer))
+        {
+            PointBehaviour pb = col.GetComponent<PointBehaviour>();
+            stats.points += pb.pointCount;
+            pb.e_die?.Invoke();
+            Destroy(pb.gameObject);
+        }
+    }
+}
