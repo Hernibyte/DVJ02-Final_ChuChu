3924b499af9cceaefd39bba47f6d88ebe4c6b442
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-03-01 21:03:50 -0300

    Added menu game manager

---
+++ Assets/Scripts/MenuGameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuGameManager : MonoBehaviour
+{
+    [SerializeField] GameObject go_credits;
+
+    void Start()
+    {
+        go_credits.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Credits(bool state)
+    {
+        go_credits.SetActive(state);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
